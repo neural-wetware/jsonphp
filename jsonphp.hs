70204b267657b08bb204d6053e400eb536b0d2e6
Daniel Lagos <dlagos@formbay.com.au>
2016-11-05 21:58:12 +1100

    Implement conversion for bools and nulls.

--- jsonphp.hs
+++ jsonphp.hs
@@ -3,6 +3,7 @@
 import Data.Vector as V (toList)
 import Data.HashMap.Strict as HM (toList)
 import Data.Aeson
+import Data.Bool (bool)
 import Data.Scientific (formatScientific, FPFormat (Fixed))
 import Data.Text (Text, unpack)
 import Data.List
@@ -21,8 +22,8 @@ xxx Nothing = "ERROR"
 phpValue :: Int -> Value -> String
 phpValue d (String v) = "\'" ++ unpack v ++ "\'"
 phpValue d (Number v) = (formatScientific Fixed (Just 0) v) -- may break for complex num formats
--- phpValue d (Bool v) = show v
--- phpValue d Null = show v
+phpValue d (Bool v) = bool "false" "true" v
+phpValue d Null = "null"
 phpValue d (Object v) = "[" ++ (ovals (d+1) v) ++ (itemIndent d) ++ "]"
 phpValue d (Array v) = "[" ++ (vals (d+1) v) ++ (itemIndent d) ++ "]"
 
